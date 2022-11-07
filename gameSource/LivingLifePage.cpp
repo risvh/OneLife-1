2444b72118b33a5cd549162a422fc0a2b2d386f0
Ira Casper <Ira.Casper@gmail.com>
2022-11-07 13:13:57 -0700

    Add dontEatTheFruit, a setting to make Eve infertile.
    If dontEatTheFruit is >0, the player is Eve on connecting, and is still younger than 15, NO BB will be said automatically.

--- gameSource/LivingLifePage.cpp
+++ gameSource/LivingLifePage.cpp
@@ -20539,6 +20539,14 @@ void LivingLifePage::step() {
                                             ancestor->isGeneticFamily = true;
                                             }
                                         }
+                                    int dontEatTheFruit = SettingsManager::getIntSetting( "dontEatTheFruit", -1 );
+                                    // TODO: If fertility age is ever not hard coded, maybe put it here?
+                                    if (dontEatTheFruit > 0 and existing->lineageEveID == ourID and existing->age < 15.0) {
+                                        // We are Eve, not yet fertile, and have chosen not to eat the fruit. No BB plz.
+                                        Thread::staticSleep( 1000 ); // Server ignores say commands sent in the first second after creation.
+                                        sendToServerSocket((char*)"SAY 0 0 NO BB#");
+                                        printf("dontEatTheFruit set, making Eve infertile.\n");
+                                        }
                                     }
                                 else {
                                     // are we an ancestor of this person?
