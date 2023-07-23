ab61e9dbe029e512b723743494e419308a245d35
risvh <67486979+risvh@users.noreply.github.com>
2023-07-23 17:41:36 +0000

    Client-side support for 0-useDistance objects

--- gameSource/LivingLifePage.cpp
+++ gameSource/LivingLifePage.cpp
@@ -24396,6 +24396,9 @@ void LivingLifePage::pointerDown( float inX, float inY ) {
 
     LiveObject *ourLiveObject = getOurLiveObject();
     
+    // allow this now, there could be ways other than 
+    // bare-ground transition to drop the object
+    if( false )
     if( ourLiveObject->holdingID > 0 &&
         getObject( ourLiveObject->holdingID )->speedMult == 0 ) {
         // holding something that stops movement entirely, ignore click
@@ -25291,7 +25294,8 @@ void LivingLifePage::pointerDown( float inX, float inY ) {
         
         char canExecute = false;
         char sideAccess = false;
-        char noBackAccess = false;
+		char noBackAccess = false;
+        char requireExactTileUsage = false;
         
         if( destID > 0 && getObject( destID )->sideAccess ) {
             sideAccess = true;
@@ -25301,6 +25305,9 @@ void LivingLifePage::pointerDown( float inX, float inY ) {
             noBackAccess = true;
             }
         
+        if( destID > 0 && getObject( destID )->useDistance == 0 ) {
+            requireExactTileUsage = true;
+            }
 
         // direct click on adjacent cells or self cell?
         if( isGridAdjacent( clickDestX, clickDestY,
@@ -25320,6 +25327,13 @@ void LivingLifePage::pointerDown( float inX, float inY ) {
             if( noBackAccess &&
                 ( clickDestY < ourLiveObject->yd ) ) {
                 // trying to access noBackAccess object from N
+                canExecute = false;
+                }
+            if( requireExactTileUsage &&
+                ( clickDestY != ourLiveObject->yd ||
+                  clickDestX != ourLiveObject->xd ) ) {
+                // using a 0 useDistance object while not standing
+                // on the exact same tile
                 canExecute = false;
                 }
 
@@ -25380,6 +25394,11 @@ void LivingLifePage::pointerDown( float inX, float inY ) {
             else if( noBackAccess ) {
                 // don't consider N neighbor
                 nLimit = 4;
+                }
+            else if( requireExactTileUsage ) {
+                // object needs to be used while
+                // standing on the exact same tile
+                nStart = 0;
                 }
             else if( destID > 0 &&
                      ( ourLiveObject->holdingID <= 0 ||
